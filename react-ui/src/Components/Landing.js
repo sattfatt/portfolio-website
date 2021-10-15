b5cbc6b928efd3387f9ae077aa222cfaaf88ab6d
sattfatt <sattfatt@gmail.com>
2021-10-15 00:59:50 -0500

    Added video background and some other tweaks

--- react-ui/src/Components/Landing.js
+++ react-ui/src/Components/Landing.js
@@ -2,22 +2,29 @@ import GdImage from "./GDriveImage";
 import Lotus from "./Lotus";
 import "../Styles/Landing.css";
 import DownArrow from "./DownArrow";
+import cubes from './FancyCubes.mp4'
 
 function Landing() {
 
     return (
-        <div className="full-page-view">
-            <div className="flex-row-lotus"><Lotus></Lotus></div>
-            <h1>It's me Satyam!</h1>
-            <em>I like to make stuff. Doesn't matter what it is, as long as it's something!</em>
-            <div className="down-arrow" onClick={scrollToFirst}><DownArrow></DownArrow></div>
-        </div>
+        <>
+            <div className="background-video-landing">
+                <video className="fancy-cubes" src={cubes} type="video/mp4" autoPlay loop muted></video>
+            </div>
+            <div className="full-page-view">
+
+                <div className="flex-row-lotus ontop"><Lotus></Lotus></div>
+                <h1 className="ontop">It's me Satyam!</h1>
+                <em className="ontop">I like to make stuff. Doesn't matter what it is, as long as it's something!</em>
+                <div className="down-arrow ontop" onClick={scrollToFirst}><DownArrow></DownArrow></div>
+            </div>
+        </>
     );
 }
 
 function scrollToFirst() {
     const el = document.getElementById('scroll-to-first');
-    el.scrollIntoView({behavior:"smooth"});
+    el.scrollIntoView({ behavior: "smooth" });
 }
 
 export default Landing;
