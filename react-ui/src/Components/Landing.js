c663a4b047baf3ca8ac73a20667d9142245eb0b6
sattfatt <satyam.p.patel94@gmail.com>
2021-12-01 12:40:56 -0600

    Added email

--- react-ui/src/Components/Landing.js
+++ react-ui/src/Components/Landing.js
@@ -16,6 +16,7 @@ function Landing() {
                 <div className="flex-row-lotus ontop"><Lotus></Lotus></div>
                 <h1 className="ontop">It's me Satyam!</h1>
                 <em className="ontop">I like to make stuff. Doesn't matter what it is, as long as it's something!</em>
+                <em className="ontop"><br/>satyam.p.patel94@gmail.com</em>
                 <div className="down-arrow ontop" onClick={scrollToFirst}><DownArrow></DownArrow></div>
             </div>
         </>
